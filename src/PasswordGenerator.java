4df605ff8542b82f9bebfad502ceaa0639538199
Christophe KUNZLI <Christophe.KUNZLI@cpnv.ch>
2023-08-10 14:32:26 +0200

    Move logic to other class

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -1,34 +1,58 @@
-public class PasswordGenerator {
-    public static void main(String[] args) {
-        int[] digits = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+import java.util.ArrayList;
+import java.util.List;
 
-        // Generate passwords with 4 distinct digits
-        for (int d1 : digits) {
-            for (int d2 : digits) {
-                if (d2 != d1) {
-                    for (int d3 : digits) {
-                        if (d3 != d1 && d3 != d2) {
-                            for (int d4 : digits) {
-                                if (d4 != d1 && d4 != d2 && d4 != d3) {
-                                    System.out.println("" + d1 + d2 + d3 + d4);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+public class PasswordGenerator {
+    static void printPasswords(List<List<Integer>> passwords) {
+        for (List<Integer> password : passwords) {
+            System.out.println(password);
+        }
+    }
+
+    /**
+     * Generates a list of possible passwords from a list of digits.
+     * Every password generated must have a length of 4 digits
+     * @param digits digits used in the password
+     * @return The full list of possible passwords
+     */
+    public static List<List<Integer>> generatePossiblePasswords(int[] digits) {
+        List<List<Integer>> possiblePasswords = new ArrayList<>();
+
+        if (digits.length == 4) {
+            generatePermutations(new ArrayList<>(), digits, new boolean[digits.length], possiblePasswords);
+        } else if (digits.length == 3) {
+            int d1 = digits[0];
+            int d2 = digits[1];
+            int d3 = digits[2];
+
+            int[] arr1 = {d1, d1, d2, d3};
+            int[] arr2 = {d1, d2, d2, d3};
+            int[] arr3 = {d1, d2, d3, d3};
+
+            generatePermutations(new ArrayList<>(), arr1, new boolean[arr1.length], possiblePasswords);
+            generatePermutations(new ArrayList<>(), arr2, new boolean[arr2.length], possiblePasswords);
+            generatePermutations(new ArrayList<>(), arr3, new boolean[arr3.length], possiblePasswords);
+        } else {
+            throw new RuntimeException("number of digits must be 3 or 4");
         }
 
-        // Generate passwords with 3 distinct digits
-        for (int d1 : digits) {
-            for (int d2 : digits) {
-                if (d2 != d1) {
-                    for (int d3 : digits) {
-                        if (d3 != d1 && d3 != d2) {
-                            System.out.println("" + d1 + d2 + d3 + d3);
-                        }
-                    }
-                }
+        return possiblePasswords;
+    }
+
+    private static void generatePermutations(List<Integer> currentPassword, int[] digits, boolean[] used, List<List<Integer>> possiblePasswords) {
+        if (currentPassword.size() == 4) {
+            possiblePasswords.add(new ArrayList<>(currentPassword));
+            return;
+        }
+
+        for (int i = 0; i < digits.length; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                currentPassword.add(digits[i]);
+
+                generatePermutations(currentPassword, digits, used, possiblePasswords);
+
+                used[i] = false;
+                currentPassword.remove(currentPassword.size() - 1);
             }
         }
     }
